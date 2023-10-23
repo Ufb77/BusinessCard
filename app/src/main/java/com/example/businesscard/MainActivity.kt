2b2a5e90be4ba56599d8edac184a990c20884341
Jose Antonio Fdez-Montes Garcia <jafmgdam@gmail.com>
2023-10-23 23:15:01 +0200

    Primera version

---
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.layout.ContentScale.Companion.FillBounds
+import androidx.compose.ui.layout.ContentScale.Companion.FillWidth
+import androidx.compose.ui.layout.ContentScale.Companion.Fit
+import androidx.compose.ui.layout.ContentScale.Companion.None
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Cabecera("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Cabecera(name: String, modifier: Modifier = Modifier) {
+
+    val backgroundHead = painterResource(R.drawable.headbackgroundcrop)
+    val myPicture = painterResource(R.drawable.me)
+
+    Box(
+        modifier = Modifier,
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            painter = backgroundHead,
+            contentDescription = null,
+            alignment = Alignment.TopCenter,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ){
+
+            Image(
+                painter = myPicture,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply {
+                    setToSaturation(0f)
+                }),
+                modifier = Modifier
+                    .padding(top = 50.dp, bottom = 20.dp)
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                alignment = Alignment.Center
+
+            )
+            Text(
+                text = "José Antonio Fernández-Montes García",
+                modifier = Modifier.padding(top = 5.dp).background(Color.LightGray)
+            )
+        }
+
+
+
+    }
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BusinessCardTheme {
+
+        Cabecera("Android", Modifier.fillMaxSize())
+    }
+}
