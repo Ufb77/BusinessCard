d6fe1858fe462aaef2f8e53c084de04ec8d11c31
Jose Antonio Fdez-Montes Garcia <jafmgdam@gmail.com>
2023-10-25 20:03:07 +0200

    Versión falta arreglar y add detalles

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -26,11 +27,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
@@ -55,8 +61,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Cabecera(name: String, modifier: Modifier = Modifier) {
 
-    val background = painterResource(R.drawable.pexels_dlkr_5676478)
+    val background = painterResource(R.drawable.pexels_laura_tancredi_7078033)
     val myPicture = painterResource(R.drawable.me)
+    val email = painterResource(R.drawable.mail_fill0_wght400_grad0_opsz24)
+    val studies = painterResource(R.drawable.school_fill0_wght400_grad0_opsz24)
+    val company = painterResource(R.drawable.storefront_fill0_wght400_grad0_opsz24)
+    val web = painterResource(R.drawable.web_fill0_wght400_grad0_opsz24)
     val infiniteTransition = rememberInfiniteTransition(label = "infinite")
     val color by infiniteTransition.animateColor(
         initialValue = Color.Green,
@@ -72,14 +82,10 @@ fun Cabecera(name: String, modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize()
     ){
         Image(
-            painter = painterResource(R.drawable.pexels_dlkr_5676478) ,
+            painter = painterResource(R.drawable.pexels_laura_tancredi_7078033) ,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .blur(
-                    radius = 5.dp,
-                    edgeTreatment = BlurredEdgeTreatment.Rectangle
-                )
+
 
         )
 
@@ -87,7 +93,7 @@ fun Cabecera(name: String, modifier: Modifier = Modifier) {
 
             modifier = Modifier
                 .align(Alignment.TopCenter)
-                .padding(top = 10.dp),
+                .padding(top = 60.dp),
             horizontalAlignment = Alignment.CenterHorizontally
 
 
@@ -104,8 +110,86 @@ fun Cabecera(name: String, modifier: Modifier = Modifier) {
 
             Text(
                 text = "José Antonio Fernández-Montes García",
-                modifier = Modifier.padding(top = 10.dp)
+                modifier = Modifier.padding(top = 10.dp),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(3.5f, 6.0f),
+                        blurRadius = 3f
+                    )
+                )
             )
+
+            Row (
+                modifier = Modifier.padding(top = 100.dp)
+            ){
+
+                Image(
+                    painter = email,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(40.dp)
+
+                )
+
+                Text(
+                    text = "jafmgdam@gmail.com",
+                    modifier = Modifier.padding(start = 5.dp, end = 10.dp, top = 9.dp),
+                    fontSize = 14.sp
+                )
+
+                Image(
+                    painter = company,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(40.dp)
+
+                )
+
+                Text(
+                    text = "www.google.com",
+                    modifier = Modifier
+                        .padding(start = 5.dp, top = 9.dp),
+                    fontSize = 14.sp
+                )
+
+            }
+
+            Row (
+                modifier = Modifier.padding(top = 100.dp)
+            ){
+
+                Image(
+                    painter = studies,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(40.dp)
+
+                )
+
+                Text(
+                    text = "Técnico superior desarrollo apps",
+                    modifier = Modifier.padding(start = 5.dp, end = 10.dp, top = 9.dp),
+                    fontSize = 14.sp
+                )
+
+                Image(
+                    painter = web,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(40.dp)
+
+                )
+
+                Text(
+                    text = "www.myweb.com",
+                    modifier = Modifier
+                        .padding(start = 5.dp, top = 9.dp),
+                    fontSize = 14.sp
+                )
+
+            }
         }
     }
 
@@ -121,6 +205,6 @@ fun Cabecera(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BusinessCardTheme {
 
-        Cabecera("Android", Modifier.fillMaxSize())
+        Cabecera("Android")
     }
 }
