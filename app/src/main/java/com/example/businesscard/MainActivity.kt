dd17b7a4027a0f01a1a4315dafb0a6e512df7d70
Jose <jafmgdam@gmail.com>
2023-10-31 10:39:47 +0100

    FinalVersion

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,14 +26,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -44,7 +39,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,13 +57,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+/**
+ * Funcion que junta todos los elementos visuales y los ordena para mostrar agrupados en un Box.
+ * Contiene una animación de cambio de color y una imagen de fondo
+ */
 @Composable
 fun Card(modifier: Modifier = Modifier) {
 
 
     val background = painterResource(R.drawable.pexels_laura_tancredi_7078033)
-    val qr = painterResource(R.drawable.qrlinkedin)
+
     val infiniteTransition = rememberInfiniteTransition(label = "infinite")
     val color by infiniteTransition.animateColor(
         initialValue = Color.Green,
@@ -82,11 +79,13 @@ fun Card(modifier: Modifier = Modifier) {
     )
 
     Box (
+        //Amplia el contenido del box a todo el tamaño y dibuja la animacion detrás del mismo.
+        //Hay también un padding que recorta la imagen y así crear un borde
         modifier.fillMaxSize().drawBehind { drawRect(color) }.padding(5.dp)
 
     ) {
         Image(
-            painter = painterResource(R.drawable.pexels_laura_tancredi_7078033),
+            painter = background,
             contentDescription = null,
             contentScale = ContentScale.Crop,
 
@@ -94,27 +93,21 @@ fun Card(modifier: Modifier = Modifier) {
             )
         CabeceraEstructuraPpal(nombre = stringResource(R.string.Nombre))
 
-        Image(
-            painter = qr ,
-            contentDescription = null,
-            modifier = modifier.offset(x = 90.dp, y = 500.dp).size(200.dp)
-                .border(
-                    BorderStroke(3.dp, Color.Green),
-                    RoundedCornerShape(20.dp)
-                )
-                .padding(3.dp)
-                .clip(RoundedCornerShape(15.dp))
 
-        )
     }
 
 
 
 }
 
+/**
+ * Compone una columna que abarca la cabecera y marca la estructura de los elementos (de arriba a abajo)
+ * Contiene la imagen de perfil, el nombre y el QR inferior
+ */
 @Composable
 fun CabeceraEstructuraPpal(nombre: String, modifier: Modifier = Modifier){
     val fotoCabecera = painterResource(R.drawable.me)
+    val qr = painterResource(R.drawable.qrlinkedin)
     Column(
 
         modifier = modifier
@@ -131,6 +124,7 @@ fun CabeceraEstructuraPpal(nombre: String, modifier: Modifier = Modifier){
             contentScale = ContentScale.Crop,
             modifier = modifier
                 .size(100.dp)
+                //Añade un borde redondeado a la imagen
                 .border(
                     BorderStroke(3.dp, Color.Green),
                     RoundedCornerShape(20.dp)
@@ -139,16 +133,16 @@ fun CabeceraEstructuraPpal(nombre: String, modifier: Modifier = Modifier){
                 .clip(RoundedCornerShape(20.dp))
         )
 
-//                   .clip(shape = RoundedCornerShape(20.dp))
-
 
         Text(
             text = nombre,
             modifier = Modifier.padding(top = 10.dp),
             style = TextStyle(
+                color = Color.Black,
                 fontSize = 20.sp,
                 shadow = Shadow(
                     color = Color.Green,
+                    //Genera sombra en el texto
                     offset = Offset(4.0f, 7.0f),
                     blurRadius = 3f
                 )
@@ -162,12 +156,29 @@ fun CabeceraEstructuraPpal(nombre: String, modifier: Modifier = Modifier){
         FilaEstudiosWeb(estudios = stringResource(R.string.Studies),
             myWeb = stringResource(R.string.myWeb))
 
+        Image(
+            painter = qr ,
+            contentDescription = null,
+            modifier = modifier.size(200.dp)
+                //Añade un borde redondeado a la imagen
+                .border(
+                    BorderStroke(3.dp, Color.Green),
+                    RoundedCornerShape(20.dp)
+                )
+                .padding(3.dp)
+                .clip(RoundedCornerShape(15.dp))
+
+        )
+
 
     }
 
 
 }
 
+/**
+ * Primera fila que muestra los iconos y textos de email y empresa
+ */
 @Composable
 fun FilaEmailEmpresa(email: String, empresa: String, modifier: Modifier = Modifier){
 
@@ -175,7 +186,7 @@ fun FilaEmailEmpresa(email: String, empresa: String, modifier: Modifier = Modifi
     val mail = painterResource(R.drawable.mail_fill0_wght400_grad0_opsz24)
 
     Row (
-        modifier = Modifier.padding(top = 100.dp)
+        modifier.padding(top = 100.dp)
 
 
     ){
@@ -191,7 +202,8 @@ fun FilaEmailEmpresa(email: String, empresa: String, modifier: Modifier = Modifi
         Text(
             text = email,
             modifier = Modifier.padding(start = 5.dp ,end = 10.dp, top = 9.dp),
-            fontSize = 14.sp
+            fontSize = 14.sp,
+            style = TextStyle(color = Color.Black)
         )
 
         Image(
@@ -204,15 +216,19 @@ fun FilaEmailEmpresa(email: String, empresa: String, modifier: Modifier = Modifi
 
         Text(
             text = empresa,
-            modifier = Modifier
+            modifier
                 .padding(start = 5.dp, top = 9.dp),
-            fontSize = 14.sp
+            fontSize = 14.sp,
+            style = TextStyle(color = Color.Black)
         )
 
     }
 
 }
 
+/**
+ * Segunda fila que muestra los iconos y textos de estudios y web
+ */
 @Composable
 fun FilaEstudiosWeb(estudios: String, myWeb: String, modifier: Modifier = Modifier){
 
@@ -220,7 +236,7 @@ fun FilaEstudiosWeb(estudios: String, myWeb: String, modifier: Modifier = Modifi
     val web = painterResource(R.drawable.web_fill0_wght400_grad0_opsz24)
 
     Row (
-        modifier = Modifier.padding(top = 100.dp)
+        modifier.padding(top = 100.dp, bottom = 40.dp)
     ){
 
         Image(
@@ -233,8 +249,9 @@ fun FilaEstudiosWeb(estudios: String, myWeb: String, modifier: Modifier = Modifi
 
         Text(
             text = estudios,
-            modifier = Modifier.padding( start = 10.dp, end = 10.dp, top = 9.dp),
-            fontSize = 14.sp
+            modifier = Modifier.padding( start = 10.dp, end = 20.dp, top = 9.dp),
+            fontSize = 14.sp,
+            style = TextStyle(color = Color.Black)
         )
 
         Image(
@@ -249,7 +266,8 @@ fun FilaEstudiosWeb(estudios: String, myWeb: String, modifier: Modifier = Modifi
             text = myWeb,
             modifier = Modifier
                 .padding(start = 5.dp, top = 9.dp),
-            fontSize = 14.sp
+            fontSize = 14.sp,
+            style = TextStyle(color = Color.Black)
         )
 
 
